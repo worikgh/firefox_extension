e26bea65125043a3ff2de8fd4e612559128f0156
Worik <5387413+worikgh@users.noreply.github.com>
2023-05-16 13:46:20 +1200

    Implements the "add(int, int) -> int" function

--- wasm_link_color/src/lib.rs
+++ wasm_link_color/src/lib.rs
@@ -1,40 +1,47 @@
 use wasm_bindgen::prelude::*;
-use web_sys::HtmlElement;
 
 #[wasm_bindgen]
-extern "C" {
-    type NodeList;
-
-    #[wasm_bindgen(method, structural, js_namespace = NodeList)]
-    fn item(this: &NodeList, index: u32) -> Option<HtmlElement>;
-
-    #[wasm_bindgen(method, getter, structural, js_namespace = NodeList)]
-    fn length(this: &NodeList) -> u32;
-
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(a: &str);
-
-    #[wasm_bindgen(js_namespace = window)]
-    fn document() -> web_sys::Document;
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
+// use wasm_bindgen::prelude::*;
+// use web_sys::{CssStyleDeclaration, Element, HtmlElement, Window};
 
-#[wasm_bindgen]
-pub fn change_links_color_to_green() {
-    set_links_color("green");
-}
+// #[wasm_bindgen]
+// extern "C" {
+//     type NodeList;
 
-// Helper function
-fn set_links_color(color: &str) {
-    let document = document();
-    let list = document
-        .query_selector_all("a")
-        .expect("cannot query selector");
-    for i in 0..list.length() {
-        if let Some(elem) = list.item(i) {
-            let style = elem.style();
-            style
-                .set_property("color", color)
-                .expect("cannot set style");
-        }
-    }
-}
+//     #[wasm_bindgen(method, structural, js_namespace = NodeList)]
+//     fn item(this: &NodeList, index: u32) -> Option<HtmlElement>;
+
+//     #[wasm_bindgen(method, getter, structural, js_namespace = NodeList)]
+//     fn length(this: &NodeList) -> u32;
+
+//     #[wasm_bindgen(js_namespace = console)]
+//     fn log(a: &str);
+
+//     #[wasm_bindgen(js_namespace = window)]
+//     fn window() -> Window;
+// }
+
+// #[wasm_bindgen]
+// pub fn change_links_color_to_green() {
+//     set_links_color("green");
+// }
+
+// // Helper function
+// fn set_links_color(color: &str) {
+//     let window = window();
+//     let document = window.document().expect("cannot get document");
+//     let list = document
+//         .query_selector_all("a")
+//         .expect("cannot query selector");
+//     for i in 0..list.length() {
+//         if let Some(elem) = list.item(i) {
+//             let style = elem.style();
+//             style
+//                 .set_property("color", color)
+//                 .expect("cannot set style");
+//         }
+//     }
+// }
